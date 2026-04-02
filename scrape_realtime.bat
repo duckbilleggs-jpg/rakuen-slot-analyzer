fe963d5bf2b905443629b5bd71f00bbd56ef394f
fumitaka.ikekatsu <fumitaka.ikekatsu@logiquest.co.jp>
2026-04-02 18:18:17 +0900

    feat: リアルタイムスクレイプに楽園池袋グリーンサイド・ARROW池上を追加 (5店舗対応)

--- scrape_realtime.bat
+++ scrape_realtime.bat
@@ -2,16 +2,24 @@
 chcp 65001 > nul
 cd /d "%~dp0"
 
-echo [%date% %time%] Task 1/3 started >> "%~dp0scrape_log.txt"
+echo [%date% %time%] Task 1/5 started >> "%~dp0scrape_log.txt"
 node scrape_realtime_cli.js --store tachikawa >> "%~dp0scrape_log.txt" 2>&1
-echo [%date% %time%] Task 1/3 done (code: %ERRORLEVEL%) >> "%~dp0scrape_log.txt"
+echo [%date% %time%] Task 1/5 done (code: %ERRORLEVEL%) >> "%~dp0scrape_log.txt"
 
-echo [%date% %time%] Task 2/3 started >> "%~dp0scrape_log.txt"
+echo [%date% %time%] Task 2/5 started >> "%~dp0scrape_log.txt"
 node scrape_realtime_cli.js --store sagamihara >> "%~dp0scrape_log.txt" 2>&1
-echo [%date% %time%] Task 2/3 done (code: %ERRORLEVEL%) >> "%~dp0scrape_log.txt"
+echo [%date% %time%] Task 2/5 done (code: %ERRORLEVEL%) >> "%~dp0scrape_log.txt"
 
-echo [%date% %time%] Task 3/3 started >> "%~dp0scrape_log.txt"
+echo [%date% %time%] Task 3/5 started >> "%~dp0scrape_log.txt"
 node scrape_realtime_cli.js --store kinshicho >> "%~dp0scrape_log.txt" 2>&1
-echo [%date% %time%] Task 3/3 done (code: %ERRORLEVEL%) >> "%~dp0scrape_log.txt"
+echo [%date% %time%] Task 3/5 done (code: %ERRORLEVEL%) >> "%~dp0scrape_log.txt"
+
+echo [%date% %time%] Task 4/5 started >> "%~dp0scrape_log.txt"
+node scrape_realtime_cli.js --store rakuen_ikebukuro >> "%~dp0scrape_log.txt" 2>&1
+echo [%date% %time%] Task 4/5 done (code: %ERRORLEVEL%) >> "%~dp0scrape_log.txt"
+
+echo [%date% %time%] Task 5/5 started >> "%~dp0scrape_log.txt"
+node scrape_realtime_cli.js --store arrow_ikegami >> "%~dp0scrape_log.txt" 2>&1
+echo [%date% %time%] Task 5/5 done (code: %ERRORLEVEL%) >> "%~dp0scrape_log.txt"
 
 echo. >> "%~dp0scrape_log.txt"
